ef2125a0de9b39f7bf9a6e0ef333d4d8efcb4ed6
norton-setup0 <106805106+norton-setup0@users.noreply.github.com>
2026-07-15 15:55:34 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,15 +4,11 @@ release = '1.0'
 
 extensions = []
 
-templates_path = []
+# _templates folder use करने के लिए
+templates_path = ['_templates']
+
 exclude_patterns = []
 
 html_theme = 'alabaster'
 
 html_static_path = []
-html_context = {
-    "metatags": """
-    <meta name="google-site-verification" content="zg06k_ScXIdGf-rc59-59YFMft8Mu4EiQXpoOUWOx2o" />
-    <meta name="msvalidate.01" content="A3EE272B6832F65CF70CEE88A8C0D8B5" />
-    """
-}
