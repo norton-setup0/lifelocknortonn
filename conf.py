957ab4809299e84874d94cd00b571dba4ccbc6aa
norton-setup0 <106805106+norton-setup0@users.noreply.github.com>
2026-07-15 13:59:45 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,21 +1,12 @@
-project = "Norton"
-author = "Norton"
-release = "1.0"
+project = 'Norton'
+author = 'Norton'
+release = '1.0'
 
 extensions = []
 
-templates_path = ["docs/_templates"]
-
+templates_path = []
 exclude_patterns = []
 
-html_theme = "alabaster"
+html_theme = 'alabaster'
 
-html_static_path = ["docs/_static"]
-
-html_css_files = [
-    "custom.css",
-]
-
-html_js_files = [
-    "chatbot.js",
-]
+html_static_path = []
