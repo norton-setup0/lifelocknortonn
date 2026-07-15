45417beb9329741bd8e80f9f4583ff90d895b3c6
norton-setup0 <106805106+norton-setup0@users.noreply.github.com>
2026-07-15 15:42:23 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,3 +10,9 @@ exclude_patterns = []
 html_theme = 'alabaster'
 
 html_static_path = []
+html_context = {
+    "metatags": """
+    <meta name="google-site-verification" content="zg06k_ScXIdGf-rc59-59YFMft8Mu4EiQXpoOUWOx2o" />
+    <meta name="msvalidate.01" content="A3EE272B6832F65CF70CEE88A8C0D8B5" />
+    """
+}
