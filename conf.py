7a5c6050dd25f53364475a523f5bb85983120b8e
norton-setup0 <106805106+norton-setup0@users.noreply.github.com>
2026-07-15 13:44:17 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,1 +1,13 @@
+html_theme = "alabaster"
 
+templates_path = ["docs/_templates"]
+
+html_static_path = ["docs/_static"]
+
+html_css_files = [
+    "custom.css",
+]
+
+html_js_files = [
+    "chatbot.js",
+]
